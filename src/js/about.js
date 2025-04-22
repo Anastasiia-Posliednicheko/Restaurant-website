5ee80c3d5f5c5137bdf8b3f69e2bec1d43e76073
Anastasiia Posliednichenko <anastasia29po@gmail.com>
2025-04-22 22:35:50 +0200

    hero about section

---
+++ src/js/about.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  spaceBetween: 24,         
+  navigation: {             
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  pagination: {            
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  
+  autoplay: { delay: 3000 },
+  effect: 'slide',          
+});
