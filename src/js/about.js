bd19cd02796354d111adfb14ecf0a3887840a246
Anastasiia Posliednichenko <anastasia29po@gmail.com>
2025-04-23 21:10:57 +0200

    update section

--- src/js/about.js
+++ src/js/about.js
@@ -1,27 +1,28 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
-
-
-
-
 
 const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 3,
   slidesPerGroup: 1,
-  spaceBetween: 24,         
-  navigation: {             
+  spaceBetween: 24,
+  navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-    },
-  pagination: {            
+  },
+  pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  
-  autoplay: { delay: 3000 },
-  effect: 'slide',          
+  autoplay: { 
+    delay: 3000,
+
+  },
+  effect: 'slide',      
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
 });
